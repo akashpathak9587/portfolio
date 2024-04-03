df297425cac0feb7444e343860002af0cd921d1a
akashpathak9587 <aakapa95@gmail.com>
2024-04-04 05:22:08 +0530

    animated scrollbar fixed

--- main.js
+++ main.js
@@ -4,17 +4,19 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+            document.querySelector("header").classList.add("active");
+        } else {
+            document.querySelector("header").classList.remove("active");
+        }
+    })
     if (window.innerWidth > 768) {
         document.querySelector("#hero").classList.add("active");
-
     } else {
         window.addEventListener("scroll", () => {
-            if (window.scrollY > 100) {
-                document.querySelector("header").classList.add("active");
+            if (window.scrollY > 300) {
                 document.querySelector("#hero").classList.add("active");
-
-            } else {
-                document.querySelector("header").classList.remove("active");
             }
         })
     }
